0bfe128d880fcadb2bcb14ae3fae535da6e28dbe
Alex-SuperMan <44372635+Alex-Superman@users.noreply.github.com>
2020-11-02 01:03:23 +0700

    Add files via upload

--- Polynom/Polynom.cpp
+++ Polynom/Polynom.cpp
@@ -1,7 +1,6 @@
 #include "Polynom.h"
-#include <iostream>
 
-using namespace std;
+/*Лабораторная работа 1*/
 
 //конструкто по умолчанию
 Polynom::Polynom()
@@ -94,15 +93,38 @@ double Polynom::getCoefficient(int degree)
 //изменить степень многочлена
 void Polynom::setDegree(int degree)
 {
+
 	if (degree < 0)
 		throw exception("Negative degree of the polynom");
 
-	_degree = degree;
+	//создаем массив с новой величиной степени многочлена
+	double* newCoefficints = new double[degree + 1];
 
+	//если новое значение степени больше текущей
+	if (degree >= _degree)
+	{
+		//при увеличении степени многочлена появившиеся коэффициенты равны нулю
+
+		//то из старого массива коэффициентов переносим все коэффициенты
+		for (int i = 0; i < _degree + 1; i++)
+			newCoefficints[i] = _coefficients[i];
+		//освободившиеся ячейки заполняем нулями
+		for (int i = _degree + 1; i < degree + 1; i++)
+			newCoefficints[i] = 0;
+	}
+	else
+	{
+		//при уменьшении степени многочлена старшие коэффициенты пропадают
+
+		//иначе переносим все коэффициенты кроме последних
+		for (int i = 0; i < degree + 1; i++)
+			newCoefficints[i] = _coefficients[i];
+	}
+
+	//сохраняем новый массив коэффициентов и степень
 	delete[] _coefficients;
-	_coefficients = new double[degree + 1];
-	for (int i = 0; i < degree + 1; i++)
-		_coefficients[i] = 0;
+	_coefficients = newCoefficints;
+	_degree = degree;
 }
 
 //изменить коэффициенты многочлена
@@ -127,11 +149,29 @@ void Polynom::setCoefficient(double coefficient, int degree)
 
 
 //вывод содержимого многочлена на экран
-void Polynom::show()
+char * Polynom::toString()
 {
-	for (int i = _degree; i > 0; i--)
-		cout << _coefficients[i] << "*x^" << i << " + ";
-	cout << _coefficients[0] << "*x^0";
+	//создаем результирующую строку
+	char* str = new char[1000];
+	int k = 0;
+	//пробегаемся по всем коэффициентам многочлена
+	for (int i = 0; i <= _degree; i++)
+	{
+		//каждый коэффициент переводим в строку
+		char* str_number = new char[100];
+		_itoa_s(_coefficients[i], str_number, 100, 10);
+
+		//строковое представление коэффициента переносим в результирующую строку
+		for (int j = 0; j < strlen(str_number); j++)
+			str[k++] = str_number[j];
+
+		str[k++] = ' ';
+
+		delete[] str_number;
+	}
+	str[k] = '\0';
+
+	return str;
 }
 
 //вычислить значение многочлена
@@ -146,4 +186,266 @@ double Polynom::compute(double x)
 	}
 
 	return polynomValue;
+}
+
+/*Лабораторная работа 2*/
+
+//оператор сложения
+Polynom Polynom::operator+(const Polynom& polynom)
+{
+	Polynom result;
+
+	//если степень текущего многочлена больше степени входного объекта
+	if (this->_degree >= polynom._degree)
+	{
+		//то ставим степень равную степени текущего объекта
+		result.setDegree(this->_degree);
+
+		//переносим все коэффициенты из текущего объекта
+		for (int i = 0; i < this->_degree + 1; i++)
+			result._coefficients[i] = this->_coefficients[i];
+
+		//складываем соответствующие коэффициенты многочленов
+		for (int i = 0; i < polynom._degree + 1; i++)
+			result._coefficients[i] += polynom._coefficients[i];
+	}
+	else
+	{
+		//делаем все то же самое, что и в предыдущем случае, только текущий объект и входной поменялись местами
+		result.setDegree(polynom._degree);
+
+		for (int i = 0; i < polynom._degree + 1; i++)
+			result._coefficients[i] = polynom._coefficients[i];
+
+		for (int i = 0; i < this->_degree + 1; i++)
+			result._coefficients[i] += this->_coefficients[i];
+	}
+
+	//возвращаем результирующий объект для того, чтобы можно было составлять множественные выражения
+	return result;
+}
+
+//оператор вычитания
+Polynom Polynom::operator-(const Polynom& polynom)
+{
+	Polynom result;
+
+	//если степень текущего многочлена больше степени входного объекта
+	if (this->_degree >= polynom._degree)
+	{
+		//то ставим степень равную степени текущего объекта
+		result.setDegree(this->_degree);
+
+		//переносим все коэффициенты из текущего объекта
+		for (int i = 0; i < this->_degree + 1; i++)
+			result._coefficients[i] = this->_coefficients[i];
+
+		//вычитаем соответствующие коэффициенты многочленов
+		for (int i = 0; i < polynom._degree + 1; i++)
+			result._coefficients[i] -= polynom._coefficients[i];
+	}
+	else
+	{
+		//делаем все то же самое, что и в предыдущем случае, только текущий объект и входной поменялись местами
+		result.setDegree(polynom._degree);
+
+		for (int i = 0; i < polynom._degree + 1; i++)
+			result._coefficients[i] = -polynom._coefficients[i];
+
+		for (int i = 0; i < this->_degree + 1; i++)
+			result._coefficients[i] += this->_coefficients[i];
+	}
+
+	//возвращаем результирующий объект для того, чтобы можно было составлять множественные выражения
+	return result;
+}
+
+//оператор инкремента (префиксная форма)
+Polynom operator++(Polynom& polynom)
+{
+	polynom.setDegree(polynom._degree + 1);
+
+	return polynom;
+}
+
+//оператор инкремента (постфиксная форма)
+Polynom operator++(Polynom& polynom, int)
+{
+	//возвращать будем старую версию текущего объекта
+	Polynom temp(polynom);
+
+	//увеличиваем степень на единицу
+	polynom.setDegree(polynom._degree + 1);
+
+	return temp;
+}
+
+//оператор декремента (префиксная форма)
+Polynom operator--(Polynom& polynom)
+{
+	//уменьшаем степень на единицу
+	polynom.setDegree(polynom._degree - 1);
+
+	return polynom;
+}
+
+//оператор декремента (постфиксная форма)
+Polynom operator--(Polynom& polynom, int)
+{
+	//возвращать будем старую версию текущего объекта
+	Polynom temp(polynom);
+
+	//уменьшаем степень на единицу
+	polynom.setDegree(polynom._degree - 1);
+
+	return temp;
+}
+
+//оператор для вычисления многочлена как функции
+double Polynom::operator()(double x)
+{
+	//возвращаем вычисленное значение полинома от х
+	return compute(x);
+}
+
+//оператор индексирования
+double& Polynom::operator[](int degree)
+{
+	if ((degree < 0) || (degree > _degree))
+		throw exception("Bad index");
+
+	//возвращаем коэффициент при степени degree
+	return _coefficients[degree];
+}
+
+//оператор присваивания
+Polynom& Polynom::operator=(const Polynom& polynom)
+{
+	delete[] _coefficients;
+
+	//копируем содержимое входного объекта в текущий
+	_degree = polynom._degree;
+	_coefficients = new double[_degree + 1];
+	for (int i = 0; i < _degree + 1; i++)
+		_coefficients[i] = polynom._coefficients[i];
+
+	return *this;
+
+}
+
+/*--------Лабораторная работа 3--------*/
+
+//запись в бинарный файд
+void Polynom::write(fstream& file)
+{
+	//проверяем, чтобы файл был открыт
+	if (!file.is_open())
+		return;
+
+	//сначала записываем степень многочлена, а потом его коэффициенты по очереди
+	file.write((char*)&_degree, sizeof(int));
+	for (int i = 0; i < _degree + 1; i++)
+		file.write((char*)&_coefficients[i], sizeof(double));
+}
+
+//чтение из бинарного файла
+void Polynom::read(fstream& file)
+{
+	//проверяем, чтобы файл был открыт
+	if (!file.is_open())
+		return;
+
+	int tempDegree;
+	//считываем степень
+	file.read((char*)&tempDegree, sizeof(int));
+
+	//проверяем считанную степень
+	if (tempDegree < 0)
+		throw exception("Bad degree");
+
+	_degree = tempDegree;
+	//пересоздаем массив коэффициентов (т.к. считана новая степень)
+	delete[] _coefficients;
+	_coefficients = new double[_degree + 1];
+	//считываем элементы из файла по очереди
+	for (int i = 0; i < _degree + 1; i++)
+		file.read((char*)&_coefficients[i], sizeof(double));
+}
+
+//вывод в обычный поток
+ostream& operator<<(ostream& stream, const Polynom& polynom)
+{
+	//выводим только коэффициенты
+	for (int i = 0; i < polynom._degree + 1; i++)
+		stream << polynom._coefficients[i] << " ";
+
+	//возвращаем ссылку на поток вывода
+	//это нужно для того, чтобы делать множественные выводы
+	//к примеру, cout << polynom1 << "Привет!" << 1 << endl;
+	return stream;
+}
+
+//вывод в файловый поток
+ofstream& operator<<(ofstream& stream, const Polynom& polynom)
+{
+	//сначала записываем степень многочлена (т.к. программа должна уметь корректно считывать данные из файла обратно)
+	stream << polynom._degree << " ";
+
+	//потом выводим коэффициенты
+	for (int i = 0; i < polynom._degree + 1; i++)
+		stream << polynom._coefficients[i] << " ";
+
+	//возвращаем ссылку на поток вывода
+	//это нужно для того, чтобы делать множественные выводы
+	//к примеру, cout << polynom1 << "Привет!" << 1 << endl;
+	return stream;
+}
+
+//чтение с обычного потока
+istream& operator>>(istream& stream, Polynom& polynom)
+{
+
+	int tempDegree;
+	//сначала считываем степень многочлена
+	stream >> tempDegree;
+	//проверяем считанную степень
+	if (tempDegree < 0)
+		throw exception("Bad degree");
+
+	polynom._degree = tempDegree;
+	//пересоздаем массив
+	delete[] polynom._coefficients;
+	polynom._coefficients = new double[polynom._degree + 1];
+	//потом считываем коэффициенты
+	for (int i = 0; i < polynom._degree + 1; i++)
+		stream >> polynom._coefficients[i];
+
+	//возвращаем ссылку на поток вывода
+	//это нужно для того, чтобы делать множественные вводы
+	//к примеру, cin >> polynom1 >> str >> var;
+	return stream;
+}
+
+//чтение с файлового потока
+ifstream& operator>>(ifstream& file, Polynom& polynom)
+{
+	int tempDegree;
+	//сначала считываем степень многочлена
+	file >> tempDegree;
+	//проверяем считанную степень
+	if (tempDegree < 0)
+		throw exception("Bad degree");
+
+	polynom._degree = tempDegree;
+	//пересоздаем массив
+	delete[] polynom._coefficients;
+	polynom._coefficients = new double[polynom._degree + 1];
+	//потом считываем коэффициенты
+	for (int i = 0; i < polynom._degree + 1; i++)
+		file >> polynom._coefficients[i];
+
+	//возвращаем ссылку на поток вывода
+	//это нужно для того, чтобы делать множественные вводы
+	//к примеру, cin >> polynom1 >> str >> var;
+	return file;
 }
--- Polynom/PolynomTests/PolynomTests.h
+++ Polynom/PolynomTests/PolynomTests.h
@@ -2,6 +2,8 @@
 
 #include "../Polynom.h"
 
+/*Лабораторная работа 1*/
+
 //тестирование для getter'ов не сделано, т.к. там примитивный код
 
 //тест для конструктора по умолчанию
@@ -25,3 +27,39 @@ void setCoefficientTest();
 //тест для метода вычисления многочлена
 void computeTest();
 
+/*Лабораторная работа 2*/
+
+//тест оператора +
+void addOperatorTest();
+
+//тест оператора -
+void subOperatorTest();
+
+//тест оператора ++ (префикс)
+void prefixIncrementOperatorTest();
+
+//тест оператора ++ (постфикс)
+void postfixIncrementOperatorTest();
+
+//тест оператора -- (префикс)
+void prefixDecrementOperatorTest();
+
+//тест оператора -- (постфикс)
+void postfixDecrementOperatorTest();
+
+//тест оператора ()
+void functionOperatorTest();
+
+//тест оператора []
+void indexOperatorTest();
+
+//тест оператора =
+void assignmentOperatorTest();
+
+/*Лабораторная работа 3*/
+
+//тест ввода-вывода в бинарный файл
+void binaryInputOutputTest();
+
+//тест ввода-вывода в текстовый файл
+void fileInputOutputTest();
